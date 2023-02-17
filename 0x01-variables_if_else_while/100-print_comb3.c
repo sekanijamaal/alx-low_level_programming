37a8bb8fa744b784fe5c56b0c044fb66177178df
sekanijamaal <sekanijamaal@gmail.com>
2023-02-17 15:57:02 -0800

    variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(0)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		if (n = 48; n < = 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar('');
+				}
+			}
+		}
+	}
+	putchar('\n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
