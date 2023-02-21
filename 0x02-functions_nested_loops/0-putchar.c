76e0ecb795f21a95d3f707c95752900580993a5b
sekanijamaal <sekanijamaal@gmail.com>
2023-02-21 14:09:13 -0800

    The program should return 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-	int str[] = {97, 112, 117, 116, 99, 104, 97, 114}
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+	_puchar('_');
+	_puchar('p');
+	_puchar('u');
+	_puchar('t');
+	_puchar('c');
+	_puchar('h');
+	_puchar('a');
+	_puchar('r');
 	_putchar('\n');
 	return (0);
 }
