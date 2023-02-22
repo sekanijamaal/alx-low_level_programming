b3c883bf9fb569fdbb49cea968d803e41f0553dd
sekanijamaal <sekanijamaal@gmail.com>
2023-02-22 04:20:01 -0800

    func

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,33 +13,29 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar(48)
+			for (j = 1; j <= n; j++)
 			{
-				k = j * i;
-				if (j == 0)
+				k = i * j;
+				_putchar(44)
+				_putchar(32)
+				if (k <= 9)
 				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
+					_putchar(32);
+					_putchar(32);
+					_putchar(k + 48);
+				}
+				else if (k <= 99)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
+					_putchar(32);
+					_putchar((k / 10) + 48);
+					_putchar((k % 10) + 48);
+				}
+				else
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar((k % 10 % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar(((k / 100) % 10) + 48);
+					_putchar(((k / 10) % 10) + 48);
+					_putchar((k % 10) + 48);
 				}
 			}
 			_putchar('\n');
