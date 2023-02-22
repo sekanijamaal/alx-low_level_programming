5cc217d7653bb0fc6a57f7e1eb074c739c33812c
sekanijamaal <sekanijamaal@gmail.com>
2023-02-21 17:40:44 -0800

    functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - check if char is lowercase
+ * @c: character to check if it is lowercase.
+ * Return: 1 if char is lowercase, otherwise 0
+ */
+
+int_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
