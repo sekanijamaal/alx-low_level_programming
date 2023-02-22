2305cded40383554434c800483b944d15223e3e4
sekanijamaal <sekanijamaal@gmail.com>
2023-02-21 18:59:18 -0800

    func

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,23 +6,29 @@
 
 void jack_bauer(void)
 {
-	int i, j;
+	int i, j, k, m;
 
-	i = 0;
-
-	while (i < 24)
+	for (i = 0; i <= 2; i++)
 	{
-		j + 0;
-		while (j < 60)
+		for (j = 0; j <= 9; j++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar('.');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			j++;
+			for (k = 0; k <= 5; k++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if (i == 2 && j > 3)
+					{break;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(m + '0');
+					_putchar('\n');
+				}
+			}
 		}
-		i++;
 	}
 }
+
+
