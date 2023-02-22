7996acae0e7aae26508621c8bc1a56a39fc2074f
sekanijamaal <sekanijamaal@gmail.com>
2023-02-22 04:23:30 -0800

    func

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,8 +17,8 @@ void print_times_table(int n)
 			for (j = 1; j <= n; j++)
 			{
 				k = i * j;
-				_putchar(44)
-				_putchar(32)
+				_putchar(44);
+				_putchar(32);
 				if (k <= 9)
 				{
 					_putchar(32);
