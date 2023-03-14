fa6d7289eacf580f0822f5889b4172c9cf14348d
sekanijamaal <sekanijamaal@gmail.com>
2023-03-13 19:34:13 -0700

    malloc free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,9 +3,9 @@
 #include "main.h"
 
 /**
- * _strdup - duplicate to new memory space location	
- * @str: char	
- * Return: 0		
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
  */
 
 char *_strdup(char *str)
@@ -17,18 +17,22 @@ char *_strdup(char *str)
 		return (NULL);
 
 	i = 0;
-	
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+
 	while (str[i] != '\0')
 		i++;
-	
+
 	aaa = malloc(sizeof(char) * (i + 1));
 
 	if (aaa == NULL)
 		return (NULL);
-	
-	for (r = 0; str[r]; r++)
+
+	for (r = 0; str[i]; r++)
 		aaa[r] = str[r];
-	
+
 	return (aaa);
 }
-
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
-/**
 
+/**
  * str_concat - get ends of input and add together for size
  * @s1: input one to concat
  * @s2: input two to concat
@@ -18,8 +18,8 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL)
 		s2 = "";
-	
-		i = ci = 0;
+
+	i = ci = 0;
 
 	while (s1[i] != '\0')
 		i++;
