3eef794c9db73f3160653f51c70ec12ca3d7809a
sekanijamaal <sekanijamaal@gmail.com>
2023-03-20 18:23:44 -0700

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,6 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	
 	if (d->name == NULL)
 		d->name = "(nil)";
 
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,6 +10,7 @@
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
 
 	while (s[i] != '\0')
@@ -32,6 +33,7 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
 
 	while (src[len] != '\0')
@@ -42,7 +44,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	
+
 	dest[i] = '\0';
 	return (dest);
 }
@@ -63,7 +65,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-	
+
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
